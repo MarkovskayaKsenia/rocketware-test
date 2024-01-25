1cbb5cf81c1bc1231b5125628a991997f59f8671
Ксения Марковская <k.markovskaya@stmd.pro>
2024-01-25 09:07:32 +0200

    модифицирует работу фильтра товаров

--- app/Filters/ProductFilter.php
+++ app/Filters/ProductFilter.php
@@ -2,6 +2,7 @@
 
 namespace App\Filters;
 
+use App\Models\Property;
 use Illuminate\Database\Eloquent\Builder;
 
 class ProductFilter extends AbstractFilter
@@ -19,8 +20,18 @@ class ProductFilter extends AbstractFilter
 
     public function properties(Builder $builder, $properties)
     {
+        $propertyIdsMap = Property::select('id', 'name')
+            ->whereIn('name', array_keys($properties))
+            ->pluck('id', 'name')->toArray();
+
         foreach($properties as $propertyName => $propertyValues) {
-            $builder->whereIn($propertyName, $propertyValues);
+            $builder->whereHas('attachedProperties',
+                function (Builder $q) use ($propertyIdsMap, $propertyName, $propertyValues) {
+                $q->where(function (Builder $subquery) use ($propertyIdsMap, $propertyName, $propertyValues) {
+                    $subquery->where('property_id', $propertyIdsMap[$propertyName])
+                        ->whereIn('property_value', $propertyValues);
+                });
+            });
         }
     }
 }
